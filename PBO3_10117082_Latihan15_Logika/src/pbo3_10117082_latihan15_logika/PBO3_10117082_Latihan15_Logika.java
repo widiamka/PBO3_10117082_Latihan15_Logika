916800738e697e4e17e549453da6f5e6e8ac9ccd
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:27:53 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan15_Logika/src/pbo3_10117082_latihan15_logika/PBO3_10117082_Latihan15_Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan15_logika;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM      : 10117082
+ */
+public class PBO3_10117082_Latihan15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     boolean a = true;
+     boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+        
+    }
+    
+}
